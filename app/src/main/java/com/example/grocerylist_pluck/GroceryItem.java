473d1e26dd8287c698396ea4556de7b101ddacad
Morgan <98068554+MorganPluck@users.noreply.github.com>
2026-04-06 19:23:30 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/grocerylist_pluck/GroceryItem.java
@@ -0,0 +1,13 @@
+package com.example.grocerylist_pluck;
+
+public class GroceryItem {
+    private String name;
+
+    public GroceryItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
